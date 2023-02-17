b04177e952dc223bb3e6786990e73c522a2abd4b
jereconjota <jeremiasfigueroa69@gmail.com>
2023-02-17 00:02:25 -0300

    update root endpoint

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -5,7 +5,7 @@ import { ping } from '../constrollers/index.controller.js';
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.send('API is working');
+    res.send('API is running... /ping to test db connection, api/employees to get all employees');
 });
 
 
